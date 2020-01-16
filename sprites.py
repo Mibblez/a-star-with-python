8713eb7c8fd755e3b629e6d622a3dd92fbde63f8
Anthony <abeninati01@manhattan.edu>
2020-01-16 14:02:46 -0500

    Refactor sprite classes

--- sprites.py
+++ sprites.py
@@ -2,79 +2,61 @@ import pygame
 from game_settings import *
 
 
-class WallSprite(pygame.sprite.Sprite):
+class BaseSprite(pygame.sprite.Sprite):
     def __init__(self, game, x_grid: int, y_grid: int):
-        self.groups = game.all_sprites, game.obstacles
+        self.groups = game.all_sprites
         pygame.sprite.Sprite.__init__(self, self.groups)
 
         self.image = pygame.Surface((game.TILE_SIZE, game.TILE_SIZE))
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x_grid * game.TILE_SIZE
+        self.rect.y = y_grid * game.TILE_SIZE
+
+
+class WallSprite(BaseSprite):
+    def __init__(self, game, x_grid: int, y_grid: int):
+        super().__init__(game, x_grid, y_grid)
+
+        self.groups.add(game.obstacles)
         self.image.fill(WHITE)
 
-        self.rect = self.image.get_rect()
-        self.rect.x = x_grid * game.TILE_SIZE
-        self.rect.y = y_grid * game.TILE_SIZE
 
-
-class NavStartSprite(pygame.sprite.Sprite):
+class NavStartSprite(BaseSprite):
     def __init__(self, game, x_grid: int, y_grid: int):
-        self.groups = game.all_sprites, game.nav_points
-        pygame.sprite.Sprite.__init__(self, self.groups)
+        super().__init__(game, x_grid, y_grid)
 
-        self.image = pygame.Surface((game.TILE_SIZE, game.TILE_SIZE))
+        self.groups.add(game.nav_points)
         self.image.fill(CYAN)
 
-        self.rect = self.image.get_rect()
-        self.rect.x = x_grid * game.TILE_SIZE
-        self.rect.y = y_grid * game.TILE_SIZE
 
-
-class NavEndSprite(pygame.sprite.Sprite):
+class NavEndSprite(BaseSprite):
     def __init__(self, game, x_grid: int, y_grid: int):
-        self.groups = game.all_sprites, game.nav_points
-        pygame.sprite.Sprite.__init__(self, self.groups)
+        super().__init__(game, x_grid, y_grid)
 
-        self.image = pygame.Surface((game.TILE_SIZE, game.TILE_SIZE))
+        self.groups.add(game.nav_points)
         self.image.fill(DARK_CYAN)
 
-        self.rect = self.image.get_rect()
-        self.rect.x = x_grid * game.TILE_SIZE
-        self.rect.y = y_grid * game.TILE_SIZE
 
-
-class NavPathSprite(pygame.sprite.Sprite):
+class NavPathSprite(BaseSprite):
     def __init__(self, game, x_grid: int, y_grid: int):
-        self.groups = game.all_sprites, game.nav_points
-        pygame.sprite.Sprite.__init__(self, self.groups)
+        super().__init__(game, x_grid, y_grid)
 
-        self.image = pygame.Surface((game.TILE_SIZE, game.TILE_SIZE))
+        self.groups.add(game.nav_points)
         self.image.fill(ORANGE)
 
-        self.rect = self.image.get_rect()
-        self.rect.x = x_grid * game.TILE_SIZE
-        self.rect.y = y_grid * game.TILE_SIZE
 
-
-class OpenSetSprite(pygame.sprite.Sprite):
+class OpenSetSprite(BaseSprite):
     def __init__(self, game, x_grid: int, y_grid: int):
-        self.groups = game.all_sprites, game.nav_points
-        pygame.sprite.Sprite.__init__(self, self.groups)
+        super().__init__(game, x_grid, y_grid)
 
-        self.image = pygame.Surface((game.TILE_SIZE, game.TILE_SIZE))
+        self.groups.add(game.nav_points)
         self.image.fill(GREEN)
 
-        self.rect = self.image.get_rect()
-        self.rect.x = x_grid * game.TILE_SIZE
-        self.rect.y = y_grid * game.TILE_SIZE
 
-
-class ClosedSetSprite(pygame.sprite.Sprite):
+class ClosedSetSprite(BaseSprite):
     def __init__(self, game, x_grid: int, y_grid: int):
-        self.groups = game.all_sprites, game.nav_points
-        pygame.sprite.Sprite.__init__(self, self.groups)
+        super().__init__(game, x_grid, y_grid)
 
-        self.image = pygame.Surface((game.TILE_SIZE, game.TILE_SIZE))
+        self.groups.add(game.nav_points)
         self.image.fill(RED)
-
-        self.rect = self.image.get_rect()
-        self.rect.x = x_grid * game.TILE_SIZE
-        self.rect.y = y_grid * game.TILE_SIZE
